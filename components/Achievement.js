04de155ad48a9bf5919a7e17014635a92654412d
yochan0103 <sky.infinity11@gmail.com>
2022-11-05 15:48:41 +0900

    animationを追加

--- components/Achievement.js
+++ components/Achievement.js
@@ -1,6 +1,7 @@
 import styles from "../components/styles/Achieve.module.css"
 import Image from "next/image";
 import { NextSeo } from "next-seo";
+import AnimationTrigger from "./AnimationTrigger";
 
 const Achievement = (props) => {
     return (
@@ -9,20 +10,22 @@ const Achievement = (props) => {
                 title={"成果物、制作物紹介"}
                 description={"Yoshihiro Netsu(根津 嘉大)の過去に制作したホームページを掲載。"}
             </NextSeo>
-            <div className={styles.Wrapper} id='works'>
-                <div className={styles.TitleWrapper}>
-                    <div className={styles.title}>Works</div>
-                </div>
-                <div className={styles.ContentsWrapper}>
-                    <div className={styles.ContentsTitle}>{props.title}</div>
-                    <div className={styles.ImgWrapper}>
-                        <a href={props.href}>
-                            <Image src={props.src} alt={props.alt} width={400} height={400} objectFit="contain"></Image>
-                        </a>
+            <AnimationTrigger animation={styles.animation} rootMargin="0px" triggerOnce>
+                <div className={styles.Wrapper} id='works'>
+                    <div className={styles.TitleWrapper}>
+                        <div className={styles.title}>Works</div>
                     </div>
-                    <div className={styles.sentence}>{ props.sentence }</div>
+                    <div className={styles.ContentsWrapper}>
+                        <div className={styles.ContentsTitle}>{props.title}</div>
+                        <div className={styles.ImgWrapper}>
+                            <a href={props.href}>
+                                <Image src={props.src} alt={props.alt} width={400} height={400} objectFit="contain"></Image>
+                            </a>
+                        </div>
+                        <div className={styles.sentence}>{ props.sentence }</div>
+                    </div>
                 </div>
-            </div>
+            </AnimationTrigger>
         </>
     )
 }
--- components/TimeLine.jsx
+++ components/TimeLine.jsx
@@ -7,168 +7,170 @@ import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
 import styles from './styles/TimeLine.module.css'
 import { FaBaby, FaSchool, FaGraduationCap, FaPen } from 'react-icons/fa'
-import { MdComputer } from 'react-icons/md'
+import { MdComputer } from 'react-icons/md';
+import AnimationTrigger from './AnimationTrigger';
 
 const TimeLine =() => {
   return (
     <div className={styles.ContentsWrapper} id='timeline'>
-      <div className={styles.TitleWrapper}>
-        <div className={styles.title}>
-          Biography
+      <AnimationTrigger animation={styles.animation} rootMargin="0px" triggerOnce>
+        <div className={styles.TitleWrapper}>
+          <div className={styles.title}>
+            Biography
+          </div>
         </div>
-      </div>
-      <Timeline position='alternate'> 
-        <TimelineItem>
-          <TimelineOppositeContent 
-            sx={{color:"text.secondary",
-                 fontSize:'1.5rem',
-                 '@media screen and (max-width:900px)':{
-                    fontSize:'20px'
-                 }
-          }} >
-            1997年
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <FaBaby 
-                sx={{fontSize:'1.5rem',
-                    '@media screen and (max-width:900px)':{
+        <Timeline position='alternate'> 
+          <TimelineItem>
+            <TimelineOppositeContent 
+              sx={{color:"text.secondary",
+                  fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
                       fontSize:'20px'
-                    }
-              }}/>
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent 
-            sx={{fontSize:'1.5rem',
-                "@media screen and (max-width:900px)":{
-                  fontSize:"20px"
-                }
+                  }
+            }} >
+              1997年
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <FaBaby 
+                  sx={{fontSize:'1.5rem',
+                      '@media screen and (max-width:900px)':{
+                        fontSize:'20px'
+                      }
+                }}/>
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent 
+              sx={{fontSize:'1.5rem',
+                  "@media screen and (max-width:900px)":{
+                    fontSize:"20px"
+                  }
+              }}>
+              出生
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent 
+              sx={{fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                    fontSize:'20px'
+                  }
             }}>
-            出生
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent 
-            sx={{fontSize:'1.5rem',
-                '@media screen and (max-width:900px)':{
-                  fontSize:'20px'
-                }
-          }}>
-            富山大学入学
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <FaSchool 
-                sx={{fontSize:'1.5rem',
-                    '@media screen and (max-width:900px)':{
-                      fontSize:'20px'
-                    }
-            }} />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent 
-            sx={{color:"text.secondary",
-                 fontSize:'1.5rem',
-                 '@media screen and (max-width:900px)':{
-                    fontSize:'20px'
-                  }
-          }}>
-            2016年
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent 
-            sx={{color:"text.secondary",
-                 fontSize:'1.5rem',
-                 '@media screen and (max-width:900px)':{
-                    fontSize:'20px'
-                  }
-          }}>
-            2020年
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <FaPen 
-                sx={{fontSize:'1.5rem',
-                    '@media screen and (max-width:900px)':{
-                      fontSize:'20px'
-                    }
-            }} />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent 
-            sx={{fontSize:'1.5rem',
-                '@media screen and (max-width:900px)':{
-                  fontSize:'20px'
-                }
-          }}>
-            富山大学卒業<br />富山大学大学院入学
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent 
-            sx={{fontSize:'1.5rem',
-                '@media screen and (max-width:900px)':{
-                  fontSize:'20px'
-                }
-          }}>
-            富山大学大学院卒業
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <FaGraduationCap 
-                sx={{fontSize:'1.5rem',
-                    '@media screen and (max-width:900px)':{
-                      fontSize:'20px'
-                    }
+              富山大学入学
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <FaSchool 
+                  sx={{fontSize:'1.5rem',
+                      '@media screen and (max-width:900px)':{
+                        fontSize:'20px'
+                      }
               }} />
-            </TimelineDot>
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent 
-            sx={{color:"text.secondary",
-                 fontSize:'1.5rem',
-                 '@media screen and (max-width:900px)':{
-                    fontSize:'20px'
-                  }
-          }}>
-            2022年
-          </TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineOppositeContent 
-            sx={{color:"text.secondary",
-                 fontSize:'1.5rem',
-                 '@media screen and (max-width:900px)':{
-                    fontSize:'20px'
-                  }
-          }}>
-            〜現在
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot>
-              <MdComputer 
-                sx={{fontSize:'1.5rem',
-                    '@media screen and (max-width:900px)':{
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent 
+              sx={{color:"text.secondary",
+                  fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
                       fontSize:'20px'
                     }
-            }} />
-            </TimelineDot>
-          </TimelineSeparator>
-          <TimelineContent 
-            sx={{fontSize:'1.5rem',
-                '@media screen and (max-width:900px)':{
-                  fontSize:'20px'
-                }
-          }}>
-            大手通信企業在籍
-          </TimelineContent>
-        </TimelineItem>
-      </Timeline>
-      
+            }}>
+              2016年
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent 
+              sx={{color:"text.secondary",
+                  fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                      fontSize:'20px'
+                    }
+            }}>
+              2020年
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <FaPen 
+                  sx={{fontSize:'1.5rem',
+                      '@media screen and (max-width:900px)':{
+                        fontSize:'20px'
+                      }
+              }} />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent 
+              sx={{fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                    fontSize:'20px'
+                  }
+            }}>
+              富山大学卒業<br />富山大学大学院入学
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent 
+              sx={{fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                    fontSize:'20px'
+                  }
+            }}>
+              富山大学大学院卒業
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <FaGraduationCap 
+                  sx={{fontSize:'1.5rem',
+                      '@media screen and (max-width:900px)':{
+                        fontSize:'20px'
+                      }
+                }} />
+              </TimelineDot>
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent 
+              sx={{color:"text.secondary",
+                  fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                      fontSize:'20px'
+                    }
+            }}>
+              2022年
+            </TimelineContent>
+          </TimelineItem>
+          <TimelineItem>
+            <TimelineOppositeContent 
+              sx={{color:"text.secondary",
+                  fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                      fontSize:'20px'
+                    }
+            }}>
+              〜現在
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot>
+                <MdComputer 
+                  sx={{fontSize:'1.5rem',
+                      '@media screen and (max-width:900px)':{
+                        fontSize:'20px'
+                      }
+              }} />
+              </TimelineDot>
+            </TimelineSeparator>
+            <TimelineContent 
+              sx={{fontSize:'1.5rem',
+                  '@media screen and (max-width:900px)':{
+                    fontSize:'20px'
+                  }
+            }}>
+              大手通信企業在籍
+            </TimelineContent>
+          </TimelineItem>
+        </Timeline>
+      </AnimationTrigger>
     </div>
   );
 }

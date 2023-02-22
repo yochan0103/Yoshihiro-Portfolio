3a79c8ed367f79c7fd5ce6912b4995ee1244c836
yochan0103 <sky.infinity11@gmail.com>
2023-02-23 01:25:32 +0900

    env追跡削除

--- libs/client.js
+++ libs/client.js
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: "4dhosyqkoq",
-  apiKey: "9b0b50742581465cb4c28d1b3a2b9643fc22",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
 });
